44e0e63332c8e8bb8c9ad8077e55529d5b95353f
Marcello Romero <79917440+marcellinromero@users.noreply.github.com>
2023-08-30 21:19:03 -0300

    Update verifica_numero_primo.c

--- atividade1/verifica_numero_primo.c
+++ atividade1/verifica_numero_primo.c
@@ -3,18 +3,15 @@
 
 int is_prime(int n) {
     if (n <= 1) {
-        return 0;  // Números menores ou iguais a 1 não são primos
+        return 0;  
     }
-    
     for (int i = 2; i * i <= n; i++) {
         if (n % i == 0) {
-            return 0;  // Se encontrar um divisor, não é primo
+            return 0;  
         }
     }
-    
-    return 1;  // Se não encontrou nenhum divisor, é primo
+    return 1;  
 }
-
 int main(int argc, char *argv[]) {
     if (argc != 2) {
         printf("Uso: %s <numero>\n", argv[0]);
